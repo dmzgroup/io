eb1693a87abf5ac83bef88303cabd536be2b5b37
Randall E. Barker <simstorm@mac.com>
2008-07-24 20:27:50 -0700

    added default blue color to run script when none is specfied

--- run.sh
+++ run.sh
@@ -11,4 +11,8 @@ else
    exit -1 ;
 fi
 
-$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/lua.xml $*
+if [ "$1" = "" ] ; then
+   export EXTRA_FILE=config/color/blue.xml ;
+fi
+
+$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/lua.xml $* $EXTRA_FILE
