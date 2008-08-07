ced83087fff6cbb16140b3a54cd2b677cc897b2b
Randall E. Barker <simstorm@mac.com>
2008-08-06 17:45:48 -0700

    upating event to net code to dynamically pick up supported events

--- run_no_audio.sh
+++ run_no_audio.sh
@@ -15,4 +15,4 @@ if [ "$1" = "" ] ; then
    export EXTRA_FILE=config/color/blue.xml ;
 fi
 
-$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/lua.xml $* $EXTRA_FILE
+$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/weapon.xml config/event.xml config/lua.xml $* $EXTRA_FILE
