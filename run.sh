9111dabf936c5a43bb8e058d311b426effd69a62
Randall E. Barker <simstorm@mac.com>
2008-08-27 19:14:56 -0700

    Moved the event modules into the common.xml file.

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ "$1" = "" ] ; then
    export EXTRA_FILE=config/color/blue.xml ;
 fi
 
-$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/event.xml config/weapon.xml config/lua.xml $* $EXTRA_FILE
+$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/weapon.xml config/lua.xml $* $EXTRA_FILE
