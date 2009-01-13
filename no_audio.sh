204f374d01c5087da0c9939256254dfefeba778e
Randall E. Barker <simstorm@mac.com>
2009-01-13 09:21:15 +0800

    Added missing config file.
    Signed-off-by: DMZ Develoment <github@dmzdev.org>

--- no_audio.sh
+++ no_audio.sh
@@ -6,4 +6,4 @@ if [ "$1" = "" ] ; then
    export EXTRA_FILE=config/color/blue.xml ;
 fi
 
-$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/weapon.xml config/lua.xml $* $EXTRA_FILE
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/common.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/weapon.xml config/lua.xml config/resource.xml $* $EXTRA_FILE
