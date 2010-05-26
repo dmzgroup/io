a54749d0ff0b9f79f7b51631cb552fc686097753
Randall E. Barker <simstorm@mac.com>
2010-05-26 16:23:44 -0700

    added noaa.xml file

--- run.sh
+++ run.sh
@@ -7,3 +7,4 @@ if [ "$1" = "" ] ; then
 fi
 
 $RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/net.xml config/render.xml config/simple.xml config/weapon.xml config/lua.xml config/hoverover_help.xml $* $EXTRA_FILE
+
